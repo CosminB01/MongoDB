399bb9ec47268eb5fbcd659e7e2c960f20c8fe56
CosminB01 <cosminbordeianu.2001@gmail.com>
2022-02-22 15:18:30 +0200

    Update

--- mongo.py
+++ mongo.py
@@ -1,7 +1,8 @@
 import pymongo
 import sys
+import time
+import os
 from datetime import datetime
-
 '''
 Provides the URL to connect Python to Mongodb using Pymongo. If the mongo server is local you can use the
 following string -- "conn_str = "mongodb://<user>:<password>@localhost:<the port that the server is listening to>
@@ -19,9 +20,16 @@ except Exception as e:
     print("Unable to connect.")
     sys.exit(-1)
 
+#UNIX timestamp
+now = int(time.time())
 
-dt = datetime (2022, 2, 21, 21, 10, 0 )
-timestamp = int(dt.timestamp() )
+timestamp_ro = time.strftime("%d/%m/%Y")
+
+timestamp_uk = time.strftime("%d/%m/%Y")
+
+timestamp_us = time.strftime("%m-%d-%Y")
+
+
 
 #Creates the database named 'employee_list' -- db = client.get_database("your_database_name")
 db = client.get_database("employee_list")
@@ -30,73 +38,104 @@ db = client.get_database("employee_list")
 db_employee = db.get_collection("employees")
 
 empl_1 = {
-    "timestamp" : timestamp,
+    "timestamp_UNIX" : now,
+    "timestamp_us" : timestamp_us,
+    "timestamp_uk" : timestamp_uk,
+    "timestamp_ro" : timestamp_ro,
     "_id" : "empl0001",
     "name" : "Mike",
     "age" : 23
 }
 
 empl_2 = {
-    "timestamp": timestamp,
+    "timestamp_UNIX" : now,
+    "timestamp_us" : timestamp_us,
+    "timestamp_uk" : timestamp_uk,
+    "timestamp_ro": timestamp_ro,
     "_id" : "empl0002",
     "name" : "Sarah",
     "age" : 31
 }
 
 empl_3 = {
-    "timestamp": timestamp,
+    "timestamp_UNIX" : now,
+    "timestamp_us" : timestamp_us,
+    "timestamp_uk" : timestamp_uk,
+    "timestamp_ro": timestamp_ro,
     "_id" : "empl0003",
     "name" : "John",
     "age" : 27
 }
 
 empl_4 = {
-    "timestamp": timestamp,
+    "timestamp_UNIX" : now,
+    "timestamp_us" : timestamp_us,
+    "timestamp_uk" : timestamp_uk,
+    "timestamp_ro": timestamp_ro,
     "_id" : "empl0004",
     "name" : "Beatrice",
     'age' : 33
 }
 
 empl_5 = {
-    "timestamp": timestamp,
+    "timestamp_UNIX" : now,
+    "timestamp_us" : timestamp_us,
+    "timestamp_uk" : timestamp_uk,
+    "timestamp_ro": timestamp_ro,
     "_id" : "empl0005",
     "name" : "Steve",
     "age" : "21"
 }
 
 empl_6 = {
-    "timestamp": timestamp,
+    "timestamp_UNIX" : now,
+    "timestamp_us" : timestamp_us,
+    "timestamp_uk" : timestamp_uk,
+    "timestamp_ro": timestamp_ro,
     "_id" : "empl0006",
     "name" : "Anna",
     "age" : 30
 }
 
 empl_7 = {
-    "timestamp": timestamp,
+    "timestamp_UNIX" : now,
+    "timestamp_us" : timestamp_us,
+    "timestamp_uk" : timestamp_uk,
+    "timestamp_ro": timestamp_ro,
     "_id" : "empl0007",
     "name" : "Andy",
     "age" : 29
 }
 
 empl_8 = {
-    "timestamp": timestamp,
+    "timestamp_UNIX" : now,
+    "timestamp_us" : timestamp_us,
+    "timestamp_uk" : timestamp_uk,
+    "timestamp_ro": timestamp_ro,
     "_id" : "empl0008",
     "name" : "Janna",
     "age" : 40
 }
 
 empl_9 = {
-    "timestamp": timestamp,
+    "timestamp_UNIX" : now,
+    "timestamp_us" : timestamp_us,
+    "timestamp_uk" : timestamp_uk,
+    "timestamp_ro": timestamp_ro,
     "_id" : "empl0009",
     "name" : "Kevin",
     "age" : 25
 }
 
 empl_10 = {
-    "timestamp": timestamp,
+    "timestamp_UNIX" : now,
+    "timestamp_us" : timestamp_us,
+    "timestamp_uk" : timestamp_uk,
+    "timestamp_ro": timestamp_ro,
     "_id" : "empl0010",
     "name" : "Senna",
     "age" : 19
 }
 
 db_employee.insert_many([empl_1, empl_2, empl_3, empl_4, empl_5, empl_6, empl_7, empl_8, empl_9, empl_10])
+print(empl_1, empl_2, empl_3, empl_4, empl_5, empl_6, empl_7, empl_8, empl_9, empl_10)
