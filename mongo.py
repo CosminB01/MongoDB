2f147cdaeb9b0ada7b5c00569b8c8f8f92f875c8
CosminB01 <cosminbordeianu.2001@gmail.com>
2022-03-02 21:43:34 +0200

    Functions Update

--- mongo.py
+++ mongo.py
@@ -1,3 +1,43 @@
+def format():
+    # Conversion of UNIX to romanian date format, printing it and updating it to the collections
+    Format = time.strftime("%d/%m/%Y, %H:%M:%S")
+    logging.info(Format + " --Ro Format")
+    for employee in db_employee.find():
+        db_employee.update_many({}, {"$set": {"ro_time": Format}})
+
+    # Conversion of UNIX to american date format, printing it and updating it to the collections
+    Format = time.strftime("%m-%d-%Y, %H:%M:%S")
+    logging.info(Format + " --Us Format")
+    for employee in db_employee.find():
+        db_employee.update_many({}, {"$set": {"us_time": Format}})
+
+    # Conversion of UNIX to british date format, printing it and updating it to the collections
+    Format = time.strftime("%d/%m/%Y, %H:%M:%S")
+    logging.info(Format + " --Uk Format")
+    for employee in db_employee.find():
+        db_employee.update_many({}, {"$set": {"uk_time": Format}})
+
+    logging.info("Updating the documents with the specified formats....\n Please wait!")
+
+def iterate():
+    #Iterates through the documents
+    for employee in db_employee.find():
+        logging.info(employee)
+    logging.info("Docs listed with succes!")
+
+def delete():
+    logging.info("Preparing to delete the documents with the employees that have the age bigger than 30....\n")
+    # Deletes the documents whose "age" key has values greater than 30
+    for employee in db_employee.find():
+        db_employee.delete_many({"age": {"$gt": 30}})
+    logging.info("Task finished!")
+
+def sort():
+    # Iterates through the documents and sorts them by age (ascending
+    for employee in db_employee.find({}).sort("age"):
+        logging.info(employee)
+    logging.info("Task finished...\n Exiting...")
+
 import pymongo
 import sys
 import time
@@ -10,7 +50,7 @@ logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s:%(levelname)s:%
 Provides the URL to connect Python to Mongodb using Pymongo. If the mongo server is local you can use the
 following string -- "conn_str = "mongodb://<user>:<password>@localhost:<the port that the server is listening to>
 '''
-conn_str = "mongodb://root:password@localhost:27017"
+conn_str = "mongodb://UsvEr4VX6ShZRsqW:4tYwbrdXJYpS74TZ@localhost:27017"
 
 #Creates a connection using pymongo.MongoClient
 client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000)
@@ -104,55 +144,22 @@ empl_10 = {
 #Inserts the dictionaries as db collections
 db_employee.insert_many([empl_1, empl_2, empl_3, empl_4, empl_5, empl_6, empl_7, empl_8, empl_9, empl_10])
 
+
 #Using some prints as loggers to show the UNIX time and convert it to different time formats:
 timestamp = int(time.time())
 logging.info("This is the UNIX timestamp: " + str(timestamp))
 Format = datetime.fromtimestamp(timestamp)
 logging.info("See the conversion for the UNIX timestamps from the documents down bellow: ")
 
-
-#Conversion of UNIX to romanian date format, printing it and updating it to the collections
-Format = time.strftime("%d/%m/%Y, %H:%M:%S")
-logging.info(Format + " --Ro Format")
-for employee in db_employee.find():
-    db_employee.update_many({},{"$set":{"ro_time" : Format}})
+format()
+iterate()
+delete()
+sort()
 
 
-#Conversion of UNIX to american date format, printing it and updating it to the collections
-Format = time.strftime("%m-%d-%Y, %H:%M:%S")
-logging.info(Format + " --Us Format")
-for employee in db_employee.find():
-    db_employee.update_many({},{"$set":{"us_time" : Format}})
 
 
-#Conversion of UNIX to british date format, printing it and updating it to the collections
-Format = time.strftime("%d/%m/%Y, %H:%M:%S")
-logging.info(Format + " --Uk Format")
-for employee in db_employee.find():
-    db_employee.update_many({},{"$set":{"uk_time" : Format}})
 
-
-logging.info("Updating the documents with the specified formats....\n Please wait!")
-
-#Iterates through the documents
-for employee in db_employee.find():
-    logging.info(employee)
-
-
-logging.info("Preparing to delete the documents with the employees that have the age bigger than 30....\n")
-
-#Deletes the documents whose "age" key has values greater than 30
-for employee in db_employee.find():
-    db_employee.delete_many({"age":{"$gt": 30}})
-
-
-logging.info("Task finished...\nPrinting and sorting the documents by the age of the employees...")
-
-#Iterates through the documents and sorts them by age (ascending
-for employee in db_employee.find({}).sort("age"):
-    logging.info(employee)
-
-logging.info("Task finished...\n Exiting...")
 
 
 
